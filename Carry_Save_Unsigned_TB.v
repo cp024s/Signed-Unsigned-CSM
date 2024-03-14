77e5411557f888d55b226c9b4489bc9262e97a87
Chandra Prakash <62211521+cp024s@users.noreply.github.com>
2024-03-14 15:43:34 +0530

    Carry Save Unsigned Multiplier Testbench

---
+++ Carry_Save_Unsigned_TB.v
@@ -0,0 +1,29 @@
+`include "carrysave.v"
+ 
+module carrysavetb;
+reg [3:0]a,b;
+wire[7:0]out;
+
+Carry_save test(a,b,out);
+
+initial
+begin
+#10
+a=4'b0111;
+b=4'b0111;
+#10
+a=4'b0111;
+b=4'b1100;
+#10
+a=4'b1111;
+b=4'b0011;
+#10
+a=4'b1111;
+b=4'b1111;
+
+end
+
+initial
+$monitor($time, "a=%d , b =%d , mul=%d",$signed(a),$signed(b),$signed(out));
+  
+endmodule
